80f0c94a5909914041e5569260dff98110a6e2ca
Michael G <michaelg@Michaels-MacBook-Pro.local>
2025-03-18 12:16:40 -0400

    feat(chunks-per-source): adding chunksPerSource param to search methods

--- src/search.ts
+++ src/search.ts
@@ -1,7 +1,15 @@
-import { TavilySearchOptions, TavilySearchFuncton, TavilyQNASearchFuncton, TavilyContextSearchFuncton} from "./types";
-import { post, DEFAULT_MAX_TOKENS, getMaxTokensFromList } from "./utils";
-
-
+import {
+  TavilySearchOptions,
+  TavilySearchFuncton,
+  TavilyQNASearchFuncton,
+  TavilyContextSearchFuncton,
+} from "./types";
+import {
+  post,
+  DEFAULT_MAX_TOKENS,
+  getMaxTokensFromList,
+  DEFAULT_CHUNKS_PER_SOURCE,
+} from "./utils";
 
 export function _search(apiKey: string): TavilySearchFuncton {
   return async function search(
@@ -18,10 +26,10 @@ export function _search(apiKey: string): TavilySearchFuncton {
       includeDomains: undefined,
       excludeDomains: undefined,
       maxTokens: undefined,
-      timeRange: undefined
+      timeRange: undefined,
+      chunksPerSource: DEFAULT_CHUNKS_PER_SOURCE,
     }
   ) {
-
     const {
       searchDepth,
       topic,
@@ -34,24 +42,30 @@ export function _search(apiKey: string): TavilySearchFuncton {
       includeDomains,
       excludeDomains,
       timeRange,
+      chunksPerSource,
       ...kwargs
     } = options;
 
-    const response = await post("search", {
-      query,
-      search_depth: searchDepth,
-      topic: topic,
-      days: days,
-      max_results: maxResults,
-      include_images: includeImages,
-      include_image_descriptions: includeImageDescriptions,
-      include_answer: includeAnswer,
-      include_raw_content: includeRawContent,
-      include_domains: includeDomains,
-      exclude_domains: excludeDomains,
-      time_range: timeRange,
-      ...kwargs
-    }, apiKey);
+    const response = await post(
+      "search",
+      {
+        query,
+        search_depth: searchDepth,
+        topic: topic,
+        days: days,
+        max_results: maxResults,
+        include_images: includeImages,
+        include_image_descriptions: includeImageDescriptions,
+        include_answer: includeAnswer,
+        include_raw_content: includeRawContent,
+        include_domains: includeDomains,
+        exclude_domains: excludeDomains,
+        time_range: timeRange,
+        chunks_per_source: chunksPerSource,
+        ...kwargs,
+      },
+      apiKey
+    );
 
     return {
       query,
@@ -72,12 +86,12 @@ export function _search(apiKey: string): TavilySearchFuncton {
           publishedDate: result.published_date,
         };
       }),
-      answer: response.data.answer
+      answer: response.data.answer,
     };
   };
 }
 
-export function _searchQNA(apiKey: string):  TavilyQNASearchFuncton {
+export function _searchQNA(apiKey: string): TavilyQNASearchFuncton {
   return async function searchQNA(
     query: string,
     options: TavilySearchOptions = {
@@ -91,22 +105,28 @@ export function _searchQNA(apiKey: string):  TavilyQNASearchFuncton {
       includeRawContent: false,
       includeDomains: undefined,
       excludeDomains: undefined,
-      maxTokens: undefined
+      maxTokens: undefined,
+      chunksPerSource: DEFAULT_CHUNKS_PER_SOURCE,
     }
   ) {
-    const response = await post("search", {
-      query,
-      search_depth: options.searchDepth,
-      topic: options.topic,
-      days: options.days,
-      max_results: options.maxResults,
-      include_images: false,
-      include_image_descriptions: false,
-      include_answer: true,
-      include_raw_content: false,
-      include_domains: options.includeDomains,
-      exclude_domains: options.excludeDomains,
-    }, apiKey);
+    const response = await post(
+      "search",
+      {
+        query,
+        search_depth: options.searchDepth,
+        topic: options.topic,
+        days: options.days,
+        max_results: options.maxResults,
+        include_images: false,
+        include_image_descriptions: false,
+        include_answer: true,
+        include_raw_content: false,
+        include_domains: options.includeDomains,
+        exclude_domains: options.excludeDomains,
+        chunks_per_source: options.chunksPerSource,
+      },
+      apiKey
+    );
 
     const answer = response.data.answer;
 
@@ -128,34 +148,39 @@ export function _searchContext(apiKey: string): TavilyContextSearchFuncton {
       includeRawContent: false,
       includeDomains: undefined,
       excludeDomains: undefined,
-      maxTokens: DEFAULT_MAX_TOKENS
+      maxTokens: DEFAULT_MAX_TOKENS,
+      chunksPerSource: DEFAULT_CHUNKS_PER_SOURCE,
     }
   ) {
-    const response = await post("search", {
-      query,
-      search_depth: options.searchDepth,
-      topic: options.topic,
-      days: options.days,
-      max_results: options.maxResults,
-      include_images: false,
-      include_image_descriptions: false,
-      include_answer: false,
-      include_raw_content: false,
-      include_domains: options.includeDomains,
-      exclude_domains: options.excludeDomains,
-      max_tokens: options.maxTokens
-    }, apiKey);
+    const response = await post(
+      "search",
+      {
+        query,
+        search_depth: options.searchDepth,
+        topic: options.topic,
+        days: options.days,
+        max_results: options.maxResults,
+        include_images: false,
+        include_image_descriptions: false,
+        include_answer: false,
+        include_raw_content: false,
+        include_domains: options.includeDomains,
+        exclude_domains: options.excludeDomains,
+        max_tokens: options.maxTokens,
+        chunks_per_source: options.chunksPerSource,
+      },
+      apiKey
+    );
 
     const sources = response.data?.results || [];
 
     const context = sources.map((source: any) => {
       return {
         url: source.url,
-        content: source.content
-      }
+        content: source.content,
+      };
     });
 
     return JSON.stringify(getMaxTokensFromList(context, options.maxTokens));
-
   };
 }
